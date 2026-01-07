95930c001a3491f879db653236d3a5b5b74186cb
ankitraj20616 <ankitrajthakur803@gmail.com>
2026-01-07 22:30:39 +0530

    Made changes in token verification process

--- app/security.py
+++ app/security.py
@@ -1,32 +1,36 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from jose import JWTError, jwt
+from .superbase_client import supabase
 from .config import settings
-import json
+
 
 security = HTTPBearer()
-
-JWK_PUBLIC_KEY = json.load(settings.SUPABASE_JWT_PUBLIC_KEY)
 
 def verify_supabase_jwt(
     credentials: HTTPAuthorizationCredentials = Depends(security)
 ):
     '''
-        This method will extract header from incoming request, and check for missing or malformed header
-        then decode the payload from token and return it
+    Verify token by calling Supabase API
     '''
     token = credentials.credentials
+    
     try:
-        payload = jwt.decode(
-            token,
-            JWK_PUBLIC_KEY,
-            algorithms=["ES256"],
-            audience= settings.SUPABASE_JWT_AUDIENCE,
-            issuer= settings.SUPABASE_JWT_ISSUER,
-        )
-        return payload
-    except JWTError:
+        response = supabase.auth.get_user(token)
+        if response and response.user:
+            return {
+                "sub": response.user.id,
+                "email": response.user.email,
+                "user_metadata": response.user.user_metadata,
+                "role": response.user.role
+            }
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token",
+            )
+    except Exception as e:
+        print(f"Auth Error: {str(e)}")
         raise HTTPException(
-            status_code= status.HTTP_401_UNAUTHORIZED,
-            detail= "Invalid or expire token!",
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
         )
